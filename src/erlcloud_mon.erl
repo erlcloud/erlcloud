3e14ff46b4959cfa58b28a57f6c4e652b9bebd0c
Zvi <zvi@nivertech.com>
2011-09-18 20:30:57 +0300

    defined records foe CloudWtatch

--- src/erlcloud_mon.erl
+++ src/erlcloud_mon.erl
@@ -12,8 +12,9 @@
 -module(erlcloud_mon).
 
 -export([
-    list_metrics/0,
-    put_metric_data/3
+    list_metrics/4,
+    put_metric_data/2,
+    get_metric_statistics/8
 ]).
 
 -include("erlcloud.hrl").
@@ -26,17 +27,55 @@
 -define(XMLNS_MON, "http://monitoring.amazonaws.com/doc/2010-08-01/").
 -define(API_VERSION, "2010-08-01").
 
+-define(FMT(Fmt,Args), lists:flatten(io_lib:format((Fmt),(Args)))).
+
 %%------------------------------------------------------------------------------
 %% @doc CloudWatch API - ListMetrics
+%% @see [ http://docs.amazonwebservices.com/AmazonCloudWatch/latest/APIReference/index.html?API_ListMetrics.html ]
+%%
+%% USAGE:
+%%
+%% erlcloud_mon:list_metrics("AWS/EC2", "NetworkIn", [{"InstanceType","m1.large"}], "").
+%% [[{metric_name,"NetworkIn"},
+%%   {namespace,"AWS/EC2"},
+%%   {dimensions,[[{name,"InstanceType"},{value,"m1.large"}]]}]]
+%%
 %% @end
 %%------------------------------------------------------------------------------
--spec list_metrics() -> term().
-list_metrics() ->
+-spec list_metrics(
+        Namespace       ::string(),
+        MetricName      ::string(),
+        DimensionFilter ::[{string(),string()}],
+        NextToken       ::string()
+    ) -> term().
+
+list_metrics(
+        Namespace,
+        MetricName,
+        DimensionFilter,
+        NextToken
+    ) ->
+
     Config = default_config(),
-    Doc = mon_query(Config, "ListMetrics", []),
+    Params = 
+        [{"Namespace",  Namespace}  || Namespace/=""]
+        ++
+        [{"MetricName", MetricName} || MetricName/=""]
+        ++
+        [{"NextToken", NextToken}   || NextToken/=""]
+        ++
+        lists:flatten(
+            [begin
+                {Name, Value} = lists:nth(N, DimensionFilter),
+                [{?FMT("Dimensions.member.~b.Name",  [N]), Name},
+                 {?FMT("Dimensions.member.~b.Value", [N]), Value}]
+             end
+             || N<-lists:seq(1, length(DimensionFilter))]
+        ),
+         
+    Doc = mon_query(Config, "ListMetrics", Params),
     Members = xmerl_xpath:string("/ListMetricsResponse/ListMetricsResult/Metrics/member", Doc),
     [extract_member(Member) || Member <- Members].
-    %Doc.
 
 extract_member(Node) ->
     [
@@ -55,16 +94,52 @@ extract_dimension(Node) ->
 
 %%------------------------------------------------------------------------------
 %% @doc CloudWatch API - PutMetricData
+%% @see [ http://docs.amazonwebservices.com/AmazonCloudWatch/latest/APIReference/index.html?API_PutMetricData.html ]
 %% @end
 %%------------------------------------------------------------------------------
--spec put_metric_data(Namespace::string(), MetricName::string(), Value::string()) -> term().
-put_metric_data(Namespace, MetricName, Value) ->
+-spec put_metric_data(
+        Namespace   ::string(),
+        MetricData  ::[metric_datum()]
+    ) -> term().
+
+put_metric_data(Namespace, MetricData) ->
     Config = default_config(),
     Params = 
-        [{"Namespace",                      Namespace}, 
-         {"MetricData.member.1.MetricName", MetricName},
-         {"MetricData.member.1.Value",      Value}],
+        [
+            {"Namespace",                      Namespace}
+            %,
+            %{"MetricData.member.1.MetricName", MetricName},
+            %{"MetricData.member.1.Value",      Value}
+        ],
     mon_query(Config, "PutMetricData", Params).
+
+%%------------------------------------------------------------------------------
+%% @doc CloudWatch API - GetMetricStatistics
+%% @see [ http://docs.amazonwebservices.com/AmazonCloudWatch/latest/APIReference/index.html?API_GetMetricStatistics.html ]
+%% @end
+%%------------------------------------------------------------------------------
+-spec get_metric_statistics(
+        Namespace   ::string(),
+        MetricName  ::string(),
+        StartTime   ::string(),
+        EndTime     ::string(),
+        Period      ::pos_integer(),
+        Unit        ::string(),
+        Statistics  ::[string()],
+        Dimensions  ::[string()]
+    ) -> term().
+
+get_metric_statistics(
+        Namespace,
+        MetricName,
+        StartTime,
+        EndTime,
+        Period,
+        Unit,
+        Statistics,
+        Dimensions
+    ) ->
+    todo.
 
 %%------------------------------------------------------------------------------
 mon_simple_query(Config, Action, Params) ->
