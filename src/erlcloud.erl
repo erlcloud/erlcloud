36bd0d0b5ae5fcb8308b0e2d1ce73e37704a1bd6
Irvin <irvinfly@gmail.com>
2012-03-19 16:58:48 +0800

    move all dependencies into .app.src.

--- src/erlcloud.erl
+++ src/erlcloud.erl
@@ -5,8 +5,6 @@
 
 start() ->
     application:load(?APP),
-    application:start(public_key),
-    application:start(ssl),
     {ok, Apps} = application:get_key(?APP, applications),
     lists:foreach(fun application:start/1, Apps),
     application:start(?APP).
