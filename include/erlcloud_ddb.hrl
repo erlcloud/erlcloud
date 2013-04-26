07591aba18e759b5c59f517d8d9b8bff62abdf7f
Ransom Richardson <ransomr@talko.com>
2013-04-26 20:48:31 +0000

    convert scan

--- include/erlcloud_ddb.hrl
+++ include/erlcloud_ddb.hrl
@@ -97,11 +97,11 @@
         }).
 
 -record(ddb_scan, 
-        {items :: [erlcloud_ddb:out_item()],
+        {consumed_capacity :: #ddb_consumed_capacity{},
          count :: non_neg_integer(),
-         scanned_count :: non_neg_integer(),
+         items :: [erlcloud_ddb:out_item()],
          last_evaluated_key :: erlcloud_ddb:hash_range_key(),
-         consumed_capacity_units :: number()
+         scanned_count :: non_neg_integer()
         }).
 
 -record(ddb_update_item,
