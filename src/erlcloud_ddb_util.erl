ad64a5d879f14af4fb59ce81e3ca00b652667b82
Ransom Richardson <ransomr@talko.com>
2013-05-03 18:51:13 +0000

    q_all

--- src/erlcloud_ddb_util.erl
+++ src/erlcloud_ddb_util.erl
@@ -18,16 +18,21 @@
 -include("erlcloud_ddb2.hrl").
 -include("erlcloud_aws.hrl").
 
-%%% DynamoDB Layer2 API
--export([delete_hash_key/3, delete_hash_key/4, delete_hash_key/5
+%%% DynamoDB Higher Layer API
+-export([delete_hash_key/3, delete_hash_key/4, delete_hash_key/5,
+         q_all/2, q_all/3, q_all/4
         ]).
 
 -define(BATCH_WRITE_LIMIT, 25).
 
--type table_name() :: erlcloud_ddb2:table_name().
 -type attr_name() :: erlcloud_ddb2:attr_name().
+-type conditions() :: erlcloud_ddb2:conditions().
 -type ddb_opts() :: erlcloud_ddb2:ddb_opts().
+-type key() :: erlcloud_ddb2:key().
 -type hash_key() :: erlcloud_ddb2:in_attr().
+-type out_item() :: erlcloud_ddb2:out_item().
+-type q_opts() :: erlcloud_ddb2:q_opts().
+-type table_name() :: erlcloud_ddb2:table_name().
 
 default_config() -> erlcloud_aws:default_config().
 
@@ -87,4 +92,63 @@ delete_hash_key(Table, HashKey, RangeKeyName, Opts, Config) ->
                             delete_hash_key(Table, HashKey, RangeKeyName, Opts, Config)
                     end
             end
+    end.
+
+%%%------------------------------------------------------------------------------
+%%% q_all
+%%%------------------------------------------------------------------------------
+
+-type q_all_return() :: {ok, [out_item()]} | {error, term()}.
+
+-spec q_all(table_name(), conditions()) -> q_all_return().
+q_all(Table, Conditions) ->
+    q_all(Table, Conditions, [], default_config()).
+
+-spec q_all(table_name(), conditions(), q_opts()) -> q_all_return().
+q_all(Table, Conditions, Opts) ->
+    q_all(Table, Conditions, Opts, default_config()).
+
+%%------------------------------------------------------------------------------
+%% @doc 
+%%
+%% Perform one or more Query operations to get all matching items.
+%%
+%% ===Example===
+%%
+%% `
+%% {ok, Items} =
+%%     erlcloud_ddb_util:q_all(
+%%       <<"Thread">>,
+%%       [{<<"LastPostDateTime">>, {{s, <<"20130101">>}, {s, <<"20130115">>}}, between},
+%%        {<<"ForumName">>, {s, <<"Amazon DynamoDB">>}}],
+%%       [{index_name, <<"LastPostIndex">>},
+%%        {select, all_attributes},
+%%        {consistent_read, true}]),
+%% '
+%%
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec q_all(table_name(), conditions(), q_opts(), aws_config()) -> q_all_return().
+q_all(Table, Conditions, Opts, Config) ->
+    q_all(Table, Conditions, Opts, Config, [], undefined).
+
+-spec q_all(table_name(), conditions(), q_opts(), aws_config(), [out_item()], key() | undefined) 
+           -> q_all_return().
+q_all(Table, Conditions, Opts, Config, Acc, StartKey) ->
+    case erlcloud_ddb2:q(Table, Conditions, 
+                         [{exclusive_start_key, StartKey}, {out, record} | Opts], 
+                         Config) of
+        {error, Reason} ->
+            {error, Reason};
+        {ok, #ddb2_q{last_evaluated_key = undefined, items = Items}} ->
+            %% Reverse and flatten the result. Can't use lists:flatten
+            %% because we only want to flatten one level.
+            Result = lists:foldl(
+                       fun(Batch, A) -> Batch ++ A end,
+                       [],
+                       [Items | Acc]),
+            {ok, Result};
+        {ok, #ddb2_q{last_evaluated_key = LastKey, items = Items}} ->
+            q_all(Table, Conditions, Opts, Config, [Items | Acc], LastKey)
     end.
