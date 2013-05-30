51aec89f8f389a1110bb8d71cc521a66a302b2ef
Phillip Calvin <phillip.calvin@eleostech.com>
2013-05-30 15:36:53 -0400

    Automatically repeat request to collect all results

--- test/erlcloud_sdb_tests.erl
+++ test/erlcloud_sdb_tests.erl
@@ -1,6 +1,15 @@
 -module(erlcloud_sdb_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+setup() ->
+    erlcloud_sdb:configure("fake", "fake-secret"),
+    meck:new(httpc, [unstick]).
+
+cleanup() ->
+    meck:unload(httpc).
+
+%% Helpers
+
 create_chain(Response) ->
     add_response_to_chain([], Response).
 
@@ -16,18 +25,18 @@ expect_chain([Response | Chain]) ->
 expect_chain([]) ->
     ok.
 
-setup() ->
-    erlcloud_sdb:configure("fake", "fake-secret"),
-    meck:new(httpc, [unstick]).
+parse_document(XML) ->
+    element(1, xmerl_scan:string(XML)).
 
-cleanup() ->
-    meck:unload(httpc).
-
+%% Fixtures
 single_result_response() ->
+    single_result_response("item0").
+
+single_result_response(Name) ->
     "<SelectResponse>
   <SelectResult>
     <Item>
-      <Name>item0</Name>
+      <Name>" ++ Name ++ "</Name>
       <Attribute><Name>Color</Name><Value>Black</Value></Attribute>
     </Item>
   </SelectResult>
@@ -36,6 +45,17 @@ single_result_response() ->
     <BoxUsage>0.1</BoxUsage>
   </ResponseMetadata>
 </SelectResponse>".
+
+next_token() ->
+    "rO0ABXNyACdjb20uYW1hem9uLnNkcy5RdWVyeVByb2Nlc3Nvci5Nb3JlVG9rZW7racXLnINNqwMA\nC0kAFGluaXRpYWxDb25qdW5jdEluZGV4WgAOaXNQYWdlQm91bmRhcnlKAAxsYXN0RW50aXR5SURa\nAApscnFFbmFibGVkSQAPcXVlcnlDb21wbGV4aXR5SgATcXVlcnlTdHJpbmdDaGVja3N1bUkACnVu\naW9uSW5kZXhaAA11c2VRdWVyeUluZGV4TAANY29uc2lzdGVudExTTnQAEkxqYXZhL2xhbmcvU3Ry\naW5nO0wAEmxhc3RBdHRyaWJ1dGVWYWx1ZXEAfgABTAAJc29ydE9yZGVydAAvTGNvbS9hbWF6b24v\nc2RzL1F1ZXJ5UHJvY2Vzc29yL1F1ZXJ5JFNvcnRPcmRlcjt4cAAAAAAB//////////8AAAAAAAAA\nAADl8izPAAAAAAB0ABZbMTM1NTM1MzAzNTk2NywyOTE5OTldcH5yAC1jb20uYW1hem9uLnNkcy5R\ndWVyeVByb2Nlc3Nvci5RdWVyeSRTb3J0T3JkZXIAAAAAAAAAABIAAHhyAA5qYXZhLmxhbmcuRW51\nbQAAAAAAAAAAEgAAeHB0AAlBU0NFTkRJTkd4".
+
+only_token_response() ->
+    "<?xml version=\"1.0\"?><SelectResponse xmlns=\"http://sdb.amazonaws.com/doc/2009-04-15/\"><SelectResult><NextToken>rO0ABXNyACdjb20uYW1hem9uLnNkcy5RdWVyeVByb2Nlc3Nvci5Nb3JlVG9rZW7racXLnINNqwMA\nC0kAFGluaXRpYWxDb25qdW5jdEluZGV4WgAOaXNQYWdlQm91bmRhcnlKAAxsYXN0RW50aXR5SURa\nAApscnFFbmFibGVkSQAPcXVlcnlDb21wbGV4aXR5SgATcXVlcnlTdHJpbmdDaGVja3N1bUkACnVu\naW9uSW5kZXhaAA11c2VRdWVyeUluZGV4TAANY29uc2lzdGVudExTTnQAEkxqYXZhL2xhbmcvU3Ry\naW5nO0wAEmxhc3RBdHRyaWJ1dGVWYWx1ZXEAfgABTAAJc29ydE9yZGVydAAvTGNvbS9hbWF6b24v\nc2RzL1F1ZXJ5UHJvY2Vzc29yL1F1ZXJ5JFNvcnRPcmRlcjt4cAAAAAAB//////////8AAAAAAAAA\nAADl8izPAAAAAAB0ABZbMTM1NTM1MzAzNTk2NywyOTE5OTldcH5yAC1jb20uYW1hem9uLnNkcy5R\ndWVyeVByb2Nlc3Nvci5RdWVyeSRTb3J0T3JkZXIAAAAAAAAAABIAAHhyAA5qYXZhLmxhbmcuRW51\nbQAAAAAAAAAAEgAAeHB0AAlBU0NFTkRJTkd4</NextToken></SelectResult><ResponseMetadata><RequestId>f20b398c-9744-0f0c-5b8a-41c436e41987</RequestId><BoxUsage>0.0000137200</BoxUsage></ResponseMetadata></SelectResponse>".
+
+single_result_and_token_response() ->
+    "<?xml version=\"1.0\"?><SelectResponse xmlns=\"http://sdb.amazonaws.com/doc/2009-04-15/\"><SelectResult><Item><Name>item0</Name><Attribute><Name>Color</Name><Value>Black</Value></Attribute></Item><NextToken>rO0ABXNyACdjb20uYW1hem9uLnNkcy5RdWVyeVByb2Nlc3Nvci5Nb3JlVG9rZW7racXLnINNqwMA\nC0kAFGluaXRpYWxDb25qdW5jdEluZGV4WgAOaXNQYWdlQm91bmRhcnlKAAxsYXN0RW50aXR5SURa\nAApscnFFbmFibGVkSQAPcXVlcnlDb21wbGV4aXR5SgATcXVlcnlTdHJpbmdDaGVja3N1bUkACnVu\naW9uSW5kZXhaAA11c2VRdWVyeUluZGV4TAANY29uc2lzdGVudExTTnQAEkxqYXZhL2xhbmcvU3Ry\naW5nO0wAEmxhc3RBdHRyaWJ1dGVWYWx1ZXEAfgABTAAJc29ydE9yZGVydAAvTGNvbS9hbWF6b24v\nc2RzL1F1ZXJ5UHJvY2Vzc29yL1F1ZXJ5JFNvcnRPcmRlcjt4cAAAAAAB//////////8AAAAAAAAA\nAADl8izPAAAAAAB0ABZbMTM1NTM1MzAzNTk2NywyOTE5OTldcH5yAC1jb20uYW1hem9uLnNkcy5R\ndWVyeVByb2Nlc3Nvci5RdWVyeSRTb3J0T3JkZXIAAAAAAAAAABIAAHhyAA5qYXZhLmxhbmcuRW51\nbQAAAAAAAAAAEgAAeHB0AAlBU0NFTkRJTkd4</NextToken></SelectResult><ResponseMetadata><RequestId>f20b398c-9744-0f0c-5b8a-41c436e41987</RequestId><BoxUsage>0.0000137200</BoxUsage></ResponseMetadata></SelectResponse>".
+
+%% Tests
 
 select_single_response_test() ->
     setup(),
@@ -66,3 +86,44 @@ select_503_test() ->
     ?assertEqual(1, length(Items)),
 
     cleanup().
+
+select_next_token_test() ->
+    setup(),
+    Chain = create_chain({ok, {{0, 200, ""}, [], only_token_response()}}),
+    expect_chain(add_response_to_chain(Chain, {ok, {{0, 200, ""}, [], single_result_response()}})),
+
+    Result = erlcloud_sdb:select("select"),
+    Items = proplists:get_value(items, Result),
+    ?assertEqual(1, length(Items)),
+
+    cleanup().
+
+select_next_and_failure_test() ->
+    setup(),
+    Chain = create_chain({ok, {{0, 200, ""}, [], only_token_response()}}),
+    Chain2 = add_response_to_chain(Chain, {ok, {{0, 503, "Unavailable"}, [], single_result_response()}}),
+    Chain3 = add_response_to_chain(Chain2, {ok, {{0, 200, ""}, [], single_result_response()}}),
+    expect_chain(Chain3),
+
+    Result = erlcloud_sdb:select("select"),
+    Items = proplists:get_value(items, Result),
+    ?assertEqual(1, length(Items)),
+
+    cleanup().
+
+select_two_results_test() ->
+    setup(),
+    Chain = create_chain({ok, {{0, 200, ""}, [], single_result_and_token_response()}}),
+    Chain2 = add_response_to_chain(Chain, {ok, {{0, 200, ""}, [], single_result_response("item1")}}),
+    expect_chain(Chain2),
+
+    Result = erlcloud_sdb:select("select"),
+    Items = proplists:get_value(items, Result),
+    ?assertEqual(["item0", "item1"], [proplists:get_value(name, Item) || Item <- Items]),
+
+    cleanup().
+
+extract_token_test() ->
+    ?assertEqual(next_token(), erlcloud_sdb:extract_token(parse_document(only_token_response()))),
+    ?assertEqual(next_token(), erlcloud_sdb:extract_token(parse_document(single_result_and_token_response()))),
+    ?assertEqual(done, erlcloud_sdb:extract_token(parse_document(single_result_response()))).
