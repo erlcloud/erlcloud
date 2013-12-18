d8923bdec4d205cae44ca4c475aff362ac568b00
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2013-12-18 15:04:34 +0200

    fix: dynamic R15 / R16 crypto compatibility adjustment

--- src/erlcloud_util.erl
+++ src/erlcloud_util.erl
@@ -2,25 +2,26 @@
 -export([sha_mac/2, sha256_mac/2,
          md5/1, sha256/1]).
 
--ifndef(old_hash).
 sha_mac(K, S) ->
-    crypto:hmac(sha, K, S).
+	case erlang:function_exported(crypto, hmac, 3) of
+		true  -> crypto:hmac(sha, K, S);
+		false -> crypto:sha_mac(K, S)
+	end.
+
 sha256_mac(K, S) ->
-    crypto:hmac(sha256, K, S).
+	case erlang:function_exported(crypto, hmac, 3) of
+		true  -> crypto:hmac(sha256, K, S);
+		false -> crypto:sha256(K, S)
+	end.
 
 sha256(V) ->
-    crypto:hash(sha256, V).
-md5(V) ->
-    crypto:hash(md5, V).
--else.
-sha_mac(K, S) ->
-    crypto:sha_mac(K, S).
-sha256(K, S) ->
-    crypto:sha256(K, S).
+	case erlang:function_exported(crypto, hash, 2) of
+		true  -> crypto:hash(sha256, V);
+		false -> crypto:sha256(V)
+	end.
 
-sha256(V) ->
-    crypto:sha256(V).
 md5(V) ->
-    crypto:md5(V).
--endif.
-
+	case erlang:function_exported(crypto, hash, 2) of
+		true  -> crypto:hash(md5, V);
+		false -> crypto:md5(V)
+	end.
