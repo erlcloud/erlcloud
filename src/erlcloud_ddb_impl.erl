7d9f1f93826c89116482fa69695875624ec4b49e
Ransom Richardson <ransomr@talko.com>
2014-03-24 18:09:55 +0000

    Call retry fun in more cases and with more information

--- src/erlcloud_ddb_impl.erl
+++ src/erlcloud_ddb_impl.erl
@@ -36,9 +36,10 @@
 
 -include("erlcloud.hrl").
 -include("erlcloud_aws.hrl").
+-include("erlcloud_ddb2.hrl").
 
 %% Helpers
--export([backoff/1, retry/2]).
+-export([backoff/1, retry/1, retry/2]).
 
 %% Internal impl api
 -export([request/3]).
@@ -84,7 +85,12 @@ backoff(Attempt) ->
     timer:sleep(random:uniform((1 bsl (Attempt - 1)) * 100)).
 
 -type attempt() :: {attempt, pos_integer()} | {error, term()}.
--type retry_fun() :: fun((pos_integer(), term()) -> attempt()).
+
+-type retry_fun_v1() :: fun((pos_integer(), term()) -> attempt()).
+-type retry_fun_v2() :: fun((#ddb2_error{}) -> attempt()).
+-type retry_fun() :: retry_fun_v1() | retry_fun_v2().
+
+%% Old retry function - maintained for backwards compat
 -spec retry(pos_integer(), term()) -> attempt().
 retry(Attempt, Reason) when Attempt >= ?NUM_ATTEMPTS ->
     {error, Reason};
@@ -92,13 +98,34 @@ retry(Attempt, _) ->
     backoff(Attempt),
     {attempt, Attempt + 1}.
 
+-spec retry(#ddb2_error{}) -> attempt().
+retry(#ddb2_error{attempt = Attempt} = Error) when Attempt >= ?NUM_ATTEMPTS ->
+    {error, Error#ddb2_error.reason};
+retry(#ddb2_error{should_retry = false} = Error) ->
+    {error, Error#ddb2_error.reason};
+retry(#ddb2_error{attempt = Attempt}) ->
+    backoff(Attempt),
+    {attempt, Attempt + 1}.
+
+-spec retry_fun(aws_config()) -> retry_fun_v2().
+retry_fun(#aws_config{ddb_retry = RetryFun}) when is_function(RetryFun, 1) ->
+    RetryFun;
+retry_fun(#aws_config{ddb_retry = RetryFun}) when is_function(RetryFun, 2) ->
+    fun(Error) -> retry_v1_wrap(Error, RetryFun) end.
+
+-spec retry_v1_wrap(#ddb2_error{}, retry_fun_v1()) -> attempt().
+retry_v1_wrap(#ddb2_error{should_retry = false} = Error, _) ->
+    {error, Error#ddb2_error.reason};
+retry_v1_wrap(Error, RetryFun) ->
+    RetryFun(Error#ddb2_error.attempt, Error#ddb2_error.reason).
+
 -type headers() :: [{string(), string()}].
 -spec request_and_retry(aws_config(), headers(), jsx:json_text(), attempt()) ->
                                {ok, jsx:json_term()} | {error, term()}.
 request_and_retry(_, _, _, {error, Reason}) ->
     {error, Reason};
 request_and_retry(Config, Headers, Body, {attempt, Attempt}) ->
-    RetryFun = Config#aws_config.ddb_retry,
+    RetryFun = retry_fun(Config),
     case httpc:request(post, {url(Config), Headers, "application/x-amz-json-1.0", Body},
                        [{timeout, 1000}],
                        [{body_format, binary}]) of
@@ -107,46 +134,60 @@ request_and_retry(Config, Headers, Body, {attempt, Attempt}) ->
             %% TODO check crc
             {ok, jsx:decode(RespBody)};
 
-        {ok, {{_, Status, StatusLine}, _, RespBody}} when Status >= 400 andalso Status < 500 ->
-            case client_error(Status, StatusLine, RespBody) of
-                {retry, Reason} ->
-                    request_and_retry(Config, Headers, Body, RetryFun(Attempt, Reason));
-                {error, Reason} ->
-                    {error, Reason}
-            end;
-
-        {ok, {{_, Status, StatusLine}, _, RespBody}} when Status >= 500 ->
-            request_and_retry(Config, Headers, Body, RetryFun(Attempt, {http_error, Status, StatusLine, RespBody}));
-
-        {ok, {{_, Status, StatusLine}, _, RespBody}} ->
-            {error, {http_error, Status, StatusLine, RespBody}};
-
-        {error, Reason} ->
-            %% TODO there may be some http errors, such as certificate error, that we don't want to retry
-            request_and_retry(Config, Headers, Body, RetryFun(Attempt, Reason))
+        Error ->
+            DDBError = #ddb2_error{attempt = Attempt, 
+                                   request_headers = Headers, 
+                                   request_body = Body},
+            request_and_retry(Config, Headers, Body, RetryFun(to_ddb_error(Error, DDBError)))
     end.
 
--spec client_error(pos_integer(), string(), binary()) -> {retry, term()} | {error, term()}.
-client_error(Status, StatusLine, Body) ->
+to_ddb_error({error, Reason}, DDBError) ->
+    %% TODO there may be some httpc errors, such as certificate error, that we don't want to retry
+    DDBError#ddb2_error{
+      error_type = httpc, 
+      should_retry = true,
+      reason = Reason};
+to_ddb_error({ok, {{_, Status, StatusLine}, RespHeaders, RespBody}}, DDBError) ->
+    DDBError2 = DDBError#ddb2_error{
+                  reason = {http_error, Status, StatusLine, RespBody},
+                  response_status = Status,
+                  response_status_line = StatusLine,
+                  response_headers = RespHeaders,
+                  response_body = RespBody},
+
+    if Status >= 400 andalso Status < 500 ->
+            client_error(RespBody, DDBError2);
+       Status >= 500 ->
+            DDBError2#ddb2_error{error_type = http, should_retry = true};
+       Status < 400 ->
+            DDBError2#ddb2_error{error_type = http, should_retry = false}
+    end.
+
+-spec client_error(binary(), #ddb2_error{}) -> #ddb2_error{}.
+client_error(Body, DDBError) ->
     case jsx:is_json(Body) of
         false ->
-            {error, {http_error, Status, StatusLine, Body}};
+            DDBError#ddb2_error{error_type = http, should_retry = false};
         true ->
             Json = jsx:decode(Body),
             case proplists:get_value(<<"__type">>, Json) of
                 undefined ->
-                    {error, {http_error, Status, StatusLine, Body}};
+                    DDBError#ddb2_error{error_type = http, should_retry = false};
                 FullType ->
                     Message = proplists:get_value(<<"message">>, Json, <<>>),
                     case binary:split(FullType, <<"#">>) of
-                        [_, <<"ProvisionedThroughputExceededException">> = Type] ->
-                            {retry, {Type, Message}};
-                        [_, <<"ThrottlingException">> = Type] ->
-                            {retry, {Type, Message}};
+                        [_, Type] when
+                              Type =:= <<"ProvisionedThroughputExceededException">> orelse
+                              Type =:= <<"ThrottlingException">> ->
+                            DDBError#ddb2_error{error_type = ddb, 
+                                                should_retry = true,
+                                                reason = {Type, Message}};
                         [_, Type] ->
-                            {error, {Type, Message}};
+                            DDBError#ddb2_error{error_type = ddb, 
+                                                should_retry = false,
+                                                reason = {Type, Message}};
                         _ ->
-                            {error, {http_error, Status, StatusLine, Body}}
+                            DDBError#ddb2_error{error_type = http, should_retry = false}
                     end
             end
     end.
