27e448c3108f376bb848111ee43684894148c874
Alexander Mishunin <amishunin@alertlogic.com>
2014-04-14 17:30:27 +0400

    ExternalId parameter for AssumeRole

--- src/erlcloud_sts.erl
+++ src/erlcloud_sts.erl
@@ -3,21 +3,35 @@
 -include_lib("erlcloud/include/erlcloud.hrl").
 -include_lib("erlcloud/include/erlcloud_aws.hrl").
 
--export([assume_role/4]).
+-export([assume_role/4, assume_role/5]).
 
 -define(API_VERSION, "2011-06-15").
 
 
+assume_role(AwsConfig, RoleArn, RoleSessionName, DurationSeconds) ->
+    assume_role(AwsConfig, RoleArn, RoleSessionName, DurationSeconds, undefined).
+
+
 % See http://docs.aws.amazon.com/STS/latest/APIReference/API_AssumeRole.html
--spec assume_role/4 :: (#aws_config{}, string(), string(), 900..3600) -> {#aws_config{}, proplist()}.
-assume_role(AwsConfig, RoleArn, RoleSessionName, DurationSeconds)
-    when length(RoleArn) >= 20, length(RoleSessionName) >= 2, length(RoleSessionName) =< 32 ->
-    Xml = sts_query(AwsConfig, "AssumeRole",
+-spec assume_role/5 :: (#aws_config{}, string(), string(), 900..3600, undefined | string()) -> {#aws_config{}, proplist()}.
+assume_role(AwsConfig, RoleArn, RoleSessionName, DurationSeconds, ExternalId)
+    when length(RoleArn) >= 20,
+         length(RoleSessionName) >= 2, length(RoleSessionName) =< 32,
+         DurationSeconds >= 900, DurationSeconds =< 3600 ->
+
+    Params =
         [
             {"RoleArn", RoleArn},
             {"RoleSessionName", RoleSessionName},
             {"DurationSeconds", DurationSeconds}
-        ]),
+        ],
+    ExternalIdPart =
+        case ExternalId of
+            undefined -> [];
+            _ when length(ExternalId) >= 2, length(ExternalId) =< 96 -> [{"ExternalId", ExternalId}]
+        end,
+
+    Xml = sts_query(AwsConfig, "AssumeRole", Params ++ ExternalIdPart),
 
     Creds = erlcloud_xml:decode(
         [
