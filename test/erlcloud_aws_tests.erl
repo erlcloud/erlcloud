ad1bf002ef05897aeb9481b2625e81565bc51fa0
Ransom Richardson <ransomr@talko.com>
2014-05-16 21:06:24 +0000

    fix cloudtrail and tests to use erlcloud_httpc

--- test/erlcloud_aws_tests.erl
+++ test/erlcloud_aws_tests.erl
@@ -10,33 +10,33 @@ request_test_() ->
       fun request_prot_host_port_int_test/1]}.
 
 start() ->
-    meck:new(httpc, [unstick]),
-    meck:expect(httpc, request, fun(_,_,_,_) -> {ok, {{0, 200, 0}, 0, ok}} end),
+    meck:new(erlcloud_httpc),
+    meck:expect(erlcloud_httpc, request, fun(_,_,_,_,_,_) -> {ok, {{200, "OK"}, [], ok}} end),
     ok.
 
 stop(_) ->
-    meck:unload(httpc).
+    meck:unload(erlcloud_httpc).
 
 request_default_test(_) ->
     ok = erlcloud_aws:aws_request(get, "host", "/", [], "id", "key"),
-    Url = get_url_from_history(meck:history(httpc)),
+    Url = get_url_from_history(meck:history(erlcloud_httpc)),
     test_url(https, "host", 443, "/", Url).
 
 request_prot_host_port_str_test(_) ->
     ok = erlcloud_aws:aws_request(get, "http", "host1", "9999", "/path1", [], "id", "key"),
-    Url = get_url_from_history(meck:history(httpc)),
+    Url = get_url_from_history(meck:history(erlcloud_httpc)),
     test_url(http, "host1", 9999, "/path1", Url).
 
 request_prot_host_port_int_test(_) ->
     ok = erlcloud_aws:aws_request(get, "http", "host1", 9999, "/path1", [], "id", "key"),
-    Url = get_url_from_history(meck:history(httpc)),
+    Url = get_url_from_history(meck:history(erlcloud_httpc)),
     test_url(http, "host1", 9999, "/path1", Url).
 
 % ==================
 % Internal functions
 % ==================
 
-get_url_from_history([{_, {httpc, request, [_, {Url, _}, _, _]}, _}]) ->
+get_url_from_history([{_, {erlcloud_httpc, request, [Url, _, _, _, _, _]}, _}]) ->
     Url.
 
 test_url(ExpScheme, ExpHost, ExpPort, ExpPath, Url) ->
