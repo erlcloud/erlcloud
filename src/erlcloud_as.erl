e90698d185930f0f6328e9d5046861bd1dd2e610
Jeremy Pierre <jeremy@Jeremys-MacBook-Pro.local>
2014-07-11 13:19:34 -0700

    Describe named autoscaling groups.
    Proper support of named groups now.

--- src/erlcloud_as.erl
+++ src/erlcloud_as.erl
@@ -2,7 +2,7 @@
 
 -include_lib("erlcloud/include/erlcloud_aws.hrl").
 
--export([describe_groups/0, describe_groups/2]).
+-export([describe_groups/0, describe_groups/1, describe_groups/2]).
 
 -import(erlcloud_xml, [get_integer/2, get_text/1, get_text/2]).
 
@@ -21,12 +21,34 @@
 -define(DESCRIBE_GROUPS_PATH, 
         "/DescribeAutoScalingGroupsResponse/DescribeAutoScalingGroupsResult/AutoScalingGroups/member").
 
+%% --------------------------------------------------------------------
+%% @doc Calls describe_groups([], default_configuration())
+%% @end
+%% --------------------------------------------------------------------
 describe_groups() ->
     describe_groups([], erlcloud_aws:default_config()).
 
+%% --------------------------------------------------------------------
+%% @doc describe_groups with a specific configuration OR with a 
+%% specific list of members.
+%% @end
+%% --------------------------------------------------------------------
+describe_groups(Config) when is_record(Config, aws_config) ->
+    describe_groups([], Config);
+describe_groups(GroupNames) ->
+    describe_groups(GroupNames, erlcloud_aws:default_config()).
+
+%% --------------------------------------------------------------------
+%% @doc Get descriptions of the given autoscaling groups.
+%%      The account calling this function needs permission for the
+%%      autoscaling:DescribeAutoScalingGroups action.
+%% @end
+%% --------------------------------------------------------------------
 -spec describe_groups(list(string()), aws_config()) -> {ok, term()} | {error, term()}.
-describe_groups([], Config) ->
-    case as_query(Config, "DescribeAutoScalingGroups", [], ?API_VERSION) of
+describe_groups(GN, Config) ->
+    MemberKeys = ["AutoScalingGroupNames.member." ++ integer_to_list(I) || I <- lists:seq(1, length(GN))],
+    Params = [{K, V} || {K, V} <- lists:zip(MemberKeys, GN)],
+    case as_query(Config, "DescribeAutoScalingGroups", Params, ?API_VERSION) of
         {ok, Doc} ->
             Groups = xmerl_xpath:string(?DESCRIBE_GROUPS_PATH, Doc),
             [extract_group(G) || G <- Groups];
