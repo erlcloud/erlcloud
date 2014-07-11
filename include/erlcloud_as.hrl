fb8f21ed2251400de7f228049e1e0d208ed4caed
Jeremy Pierre <jeremy@Jeremys-MacBook-Pro.local>
2014-07-11 15:14:52 -0700

    Cleanup per omarkj's advice.

--- include/erlcloud_as.hrl
+++ include/erlcloud_as.hrl
@@ -1,9 +1,9 @@
 -record(aws_autoscaling_group, {
-          group_name::string(),
-          availability_zones::list(string()),
-          tags::list(string()),
-          desired_capacity::integer(),
-          min_size::integer(),
-          max_size::integer()          
+          group_name :: string(),
+          availability_zones :: list(string()),
+          tags :: list(string()),
+          desired_capacity :: integer(),
+          min_size :: integer(),
+          max_size :: integer()          
          }).
 -type(aws_autoscaling_group()::#aws_autoscaling_group{}).
