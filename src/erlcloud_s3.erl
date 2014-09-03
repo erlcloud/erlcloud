609b858f1bc4d374ff7941e185b367f2c412d938
Heinz N. Gies <heinz@licenser.net>
2014-09-03 12:55:39 -0400

    Use binary for httpc

--- src/erlcloud_s3.erl
+++ src/erlcloud_s3.erl
@@ -384,7 +384,7 @@ get_object(BucketName, Key, Options, Config) ->
      {content_type, proplists:get_value("content-type", Headers)},
      {delete_marker, list_to_existing_atom(proplists:get_value("x-amz-delete-marker", Headers, "false"))},
      {version_id, proplists:get_value("x-amz-version-id", Headers, "null")},
-     {content, list_to_binary(Body)}|
+     {content, Body}|
      extract_metadata(Headers)].
 
 -spec get_object_acl(string(), string()) -> proplist().
@@ -461,7 +461,7 @@ get_object_torrent(BucketName, Key, Config) ->
     {Headers, Body} = s3_request(Config, get, BucketName, [$/|Key], "torrent", [], <<>>, []),
     [{delete_marker, list_to_existing_atom(proplists:get_value("x-amz-delete-marker", Headers, "false"))},
      {version_id, proplists:get_value("x-amz-delete-marker", Headers, "false")},
-     {torrent, list_to_binary(Body)}].
+     {torrent, Body}].
 
 -spec list_object_versions(string()) -> proplist().
 
@@ -822,9 +822,9 @@ encode_grant(Grant) ->
 
 s3_simple_request(Config, Method, Host, Path, Subresource, Params, POSTData, Headers) ->
     case s3_request(Config, Method, Host, Path, Subresource, Params, POSTData, Headers) of
-        {_Headers, ""} -> ok;
+        {_Headers, <<"">>} -> ok;
         {_Headers, Body} ->
-            XML = element(1,xmerl_scan:string(Body)),
+            XML = element(1,xmerl_scan:string(binary_to_list(Body))),
             case XML of
                 #xmlElement{name='Error'} ->
                     ErrCode = erlcloud_xml:get_text("/Error/Code", XML),
@@ -837,7 +837,7 @@ s3_simple_request(Config, Method, Host, Path, Subresource, Params, POSTData, Hea
 
 s3_xml_request(Config, Method, Host, Path, Subresource, Params, POSTData, Headers) ->
     {_Headers, Body} = s3_request(Config, Method, Host, Path, Subresource, Params, POSTData, Headers),
-    XML = element(1,xmerl_scan:string(Body)),
+    XML = element(1,xmerl_scan:string(binary_to_list(Body))),
     case XML of
         #xmlElement{name='Error'} ->
             ErrCode = erlcloud_xml:get_text("/Error/Code", XML),
@@ -868,7 +868,7 @@ s3_request2(Config, Method, Host, Path, Subresource, Params, POSTData, Headers) 
 s3_xml_request2(Config, Method, Host, Path, Subresource, Params, POSTData, Headers) ->
     case s3_request2(Config, Method, Host, Path, Subresource, Params, POSTData, Headers) of
 	{ok, {_Headers, Body}} ->
-	    XML = element(1,xmerl_scan:string(Body)),
+	    XML = element(1,xmerl_scan:string(binary_to_list(Body))),
 	    case XML of
 		#xmlElement{name='Error'} ->
 		    ErrCode = erlcloud_xml:get_text("/Error/Code", XML),
@@ -913,12 +913,12 @@ s3_request2_no_update(Config, Method, Host, Path, Subresource, Params, POSTData,
                                     true -> [$&, erlcloud_http:make_query_string(Params)]
                                 end
                                ]),
-
+	Opts = [{body_format, binary}],
     Response = case Method of
-                   get -> httpc:request(Method, {RequestURI, RequestHeaders}, [], []);
-                   head -> httpc:request(Method, {RequestURI, RequestHeaders}, [], []);
-                   delete -> httpc:request(Method, {RequestURI, RequestHeaders}, [], []);
-                   _ -> httpc:request(Method, {RequestURI, RequestHeaders, ContentType, Body}, [], [])
+                   get -> httpc:request(Method, {RequestURI, RequestHeaders}, Opts, []);
+                   head -> httpc:request(Method, {RequestURI, RequestHeaders}, Opts, []);
+                   delete -> httpc:request(Method, {RequestURI, RequestHeaders}, Opts, []);
+                   _ -> httpc:request(Method, {RequestURI, RequestHeaders, ContentType, Body}, Opts, [])
                end,
     erlcloud_aws:http_headers_body(Response).
 
