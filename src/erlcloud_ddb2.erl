219e01af878a8059fe1424b9e703d707293b244b
Smiler Lee <smilerlee@live.com>
2014-09-19 13:09:43 +0800

    Corrected several type specs

--- src/erlcloud_ddb2.erl
+++ src/erlcloud_ddb2.erl
@@ -350,7 +350,7 @@ dynamize_maybe_list(DynamizeItem, List) when is_list(List) ->
 dynamize_maybe_list(DynamizeItem, Item) ->
     [DynamizeItem(Item)].
 
--spec dynamize_expected_item(in_expected()) -> json_pair().
+-spec dynamize_expected_item(in_expected_item()) -> json_pair().
 dynamize_expected_item({Name, false}) ->
     {Name, [{<<"Exists">>, false}]};
 dynamize_expected_item({Name, Value}) ->
@@ -834,11 +834,11 @@ batch_get_item_record() ->
 
 -type batch_get_item_return() :: ddb_return(#ddb2_batch_get_item{}, [erlcloud_ddb:out_item()]).
 
--spec batch_get_item([batch_get_item_request_item()]) -> batch_get_item_return().
+-spec batch_get_item(batch_get_item_request_items()) -> batch_get_item_return().
 batch_get_item(RequestItems) ->
     batch_get_item(RequestItems, [], default_config()).
 
--spec batch_get_item([batch_get_item_request_item()], batch_get_item_opts()) -> batch_get_item_return().
+-spec batch_get_item(batch_get_item_request_items(), batch_get_item_opts()) -> batch_get_item_return().
 batch_get_item(RequestItems, Opts) ->
     batch_get_item(RequestItems, Opts, default_config()).
 
@@ -871,7 +871,7 @@ batch_get_item(RequestItems, Opts) ->
 %%
 %% @end
 %%------------------------------------------------------------------------------
--spec batch_get_item([batch_get_item_request_item()], batch_get_item_opts(), aws_config()) -> 
+-spec batch_get_item(batch_get_item_request_items(), batch_get_item_opts(), aws_config()) -> 
                             batch_get_item_return().
 batch_get_item(RequestItems, Opts, Config) ->
     {AwsOpts, DdbOpts} = opts(batch_get_item_opts(), Opts),
@@ -969,11 +969,11 @@ batch_write_item_record() ->
 
 -type batch_write_item_return() :: ddb_return(#ddb2_batch_write_item{}, #ddb2_batch_write_item{}).
 
--spec batch_write_item([batch_write_item_request_item()]) -> batch_write_item_return().
+-spec batch_write_item(batch_write_item_request_items()) -> batch_write_item_return().
 batch_write_item(RequestItems) ->
     batch_write_item(RequestItems, [], default_config()).
 
--spec batch_write_item([batch_write_item_request_item()], batch_write_item_opts()) -> batch_write_item_return().
+-spec batch_write_item(batch_write_item_request_items(), batch_write_item_opts()) -> batch_write_item_return().
 batch_write_item(RequestItems, Opts) ->
     batch_write_item(RequestItems, Opts, default_config()).
 
@@ -1004,7 +1004,7 @@ batch_write_item(RequestItems, Opts) ->
 %% '
 %% @end
 %%------------------------------------------------------------------------------
--spec batch_write_item([batch_write_item_request_item()], batch_write_item_opts(), aws_config()) -> 
+-spec batch_write_item(batch_write_item_request_items(), batch_write_item_opts(), aws_config()) -> 
                               batch_write_item_return().
 batch_write_item(RequestItems, Opts, Config) ->
     {AwsOpts, DdbOpts} = opts(batch_write_item_opts(), Opts),
