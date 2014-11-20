fbed6ee3738d0829a7cd79a896cd9d911e2401d4
benoitc <bchesneau@gmail.com>
2014-11-20 13:51:57 +0100

    prefer hackney to lhttpc
    note: this is just a proof of concept, it may be better to patch erlcoud to
    handle different clients instead so the patch can be accepted upstream.

--- src/erlcloud_httpc.erl
+++ src/erlcloud_httpc.erl
@@ -11,5 +11,19 @@
 
 -export([request/6]).
 
-request(URL, Method, Hdrs, Body, Timeout, _Config) ->
-    lhttpc:request(URL, Method, Hdrs, Body, Timeout, []).
+request(URL, Method, Hdrs, Body, Timeout, Config) ->
+    Pool =  proplists:get_value(hackney_pool, Config, default),
+
+    Options = [{recv_timeout, Timeout},
+               {connect_timeout, Timeout},
+               {pool, Pool}],
+
+    case hackney:request(Method, URL, Hdrs, Body, Options) of
+        {ok, Status, RespHeaders, Ref} ->
+            {ok, RespBody} = hackney:body(Ref),
+            {ok, {{Status, <<>>}, RespHeaders, RespBody}};
+        {ok, Status, RespHeaders} ->
+            {ok, {{Status, <<>>}, RespHeaders, <<>>}};
+        Error ->
+            Error
+    end.
