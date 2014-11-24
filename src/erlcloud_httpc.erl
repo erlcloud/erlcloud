e97964b91f1b098330402fa11076a666352a8c78
Guilherme Andrade <g@gandrade.net>
2014-11-24 15:29:44 +0000

    Support multiple lhttpc pools.

--- src/erlcloud_httpc.erl
+++ src/erlcloud_httpc.erl
@@ -9,7 +9,29 @@
 
 -module(erlcloud_httpc).
 
+-include("include/erlcloud_aws.hrl").
+
 -export([request/6]).
 
-request(URL, Method, Hdrs, Body, Timeout, _Config) ->
-    lhttpc:request(URL, Method, Hdrs, Body, Timeout, []).
+
+-define(LHTTPC_POOLID_PADDING, 2).
+
+request(URL, Method, Hdrs, Body, Timeout, #aws_config{ httpc_config=(#lhttpc_config{}=LHttpcConfig) }) ->
+    #lhttpc_config{
+            pools=Pools, pool_prefix=PPrefix, pool_connection_timeout=PConnTimeout, pool_max_size=PMaxSize
+        } = LHttpcConfig,
+
+    PIndex = erlang:phash2({URL, Method, Hdrs, Body, Timeout}, Pools),
+    PSuffix = case PIndex >= (?LHTTPC_POOLID_PADDING * 10) of
+        true  -> integer_to_list(PIndex);
+        false ->
+            FormatStr = lists:flatten(["~", integer_to_list(?LHTTPC_POOLID_PADDING), "..0B"]),
+            lists:flatten( io_lib:format(FormatStr, [PIndex]) )
+    end,
+    PoolId = list_to_atom(PPrefix ++ PSuffix),
+
+    LHttpcConfigParams = [{pool, PoolId},
+                          {pool_ensure, true},
+                          {pool_connection_timeout, PConnTimeout},
+                          {pool_max_size, PMaxSize}],
+    lhttpc:request(URL, Method, Hdrs, Body, Timeout, LHttpcConfigParams).
