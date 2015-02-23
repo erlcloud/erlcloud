945611bde860b2c4dfb8d8615fcbe64691d65371
Ransom Richardson <ransomr@talko.com>
2015-02-23 20:09:14 +0000

    fix indentation in erlcloud_sts:get_federation_token

--- src/erlcloud_sts.erl
+++ src/erlcloud_sts.erl
@@ -1,11 +1,12 @@
+%% -*- mode: erlang;erlang-indent-level: 4;indent-tabs-mode: nil -*-
 -module(erlcloud_sts).
 
 -include_lib("erlcloud/include/erlcloud.hrl").
 -include_lib("erlcloud/include/erlcloud_aws.hrl").
 
 -export([assume_role/4, assume_role/5,
-		get_federation_token/3,
-		get_federation_token/4]).
+         get_federation_token/3,
+         get_federation_token/4]).
 
 -define(API_VERSION, "2011-06-15").
 
@@ -55,44 +56,44 @@ assume_role(AwsConfig, RoleArn, RoleSessionName, DurationSeconds, ExternalId)
 
 
 get_federation_token(AwsConfig, DurationSeconds, Name) ->
-	get_federation_token(AwsConfig, DurationSeconds, Name, undefined).
+        get_federation_token(AwsConfig, DurationSeconds, Name, undefined).
 
 % See http://docs.aws.amazon.com/STS/latest/APIReference/API_GetFederationToken.html
 -spec get_federation_token/4 :: (#aws_config{}, 900..129600, string(), undefined | string()) -> {#aws_config{}, proplist()}.
 get_federation_token(AwsConfig, DurationSeconds, Name, Policy)
   when length(Name) >= 2, length(Name) =< 32,
-  	   DurationSeconds >= 900, DurationSeconds =< 129600 ->
+       DurationSeconds >= 900, DurationSeconds =< 129600 ->
 
-  	Params =
-  	[
-	 {"DurationSeconds", DurationSeconds},
-	 {"Name", Name}
-	],
+    Params =
+        [
+         {"DurationSeconds", DurationSeconds},
+         {"Name", Name}
+        ],
 
-	PolicyList =
-	case Policy of
-		undefined -> [];
-		_ -> [{"Policy", Policy}]
-	end,
-	Xml = sts_query(AwsConfig, "GetFederationToken", Params ++ PolicyList),
+    PolicyList =
+        case Policy of
+            undefined -> [];
+            _ -> [{"Policy", Policy}]
+        end,
+    Xml = sts_query(AwsConfig, "GetFederationToken", Params ++ PolicyList),
 
-	Creds = erlcloud_xml:decode(
-			  [
-			   {access_key_id		, "GetFederationTokenResult/Credentials/AccessKeyId", text},
-			   {secret_access_key	, "GetFederationTokenResult/Credentials/SecretAccessKey", text},
-			   {federated_user_arn  , "GetFederationTokenResult/FederatedUser/Arn", text},
-			   {federated_user_id   , "GetFederationTokenResult/FederatedUser/FederatedUserId", text}
-			  ],
-			  Xml),
+    Creds = erlcloud_xml:decode(
+              [
+               {access_key_id               , "GetFederationTokenResult/Credentials/AccessKeyId", text},
+               {secret_access_key   , "GetFederationTokenResult/Credentials/SecretAccessKey", text},
+               {federated_user_arn  , "GetFederationTokenResult/FederatedUser/Arn", text},
+               {federated_user_id   , "GetFederationTokenResult/FederatedUser/FederatedUserId", text}
+              ],
+              Xml),
 
-	FederatedConfig =
-	AwsConfig#aws_config {
-            access_key_id     = proplists:get_value(access_key_id, Creds),
-            secret_access_key = proplists:get_value(secret_access_key, Creds),
-            security_token    = proplists:get_value(session_token, Creds)
-	 },
-	
-	{FederatedConfig, Creds}.
+    FederatedConfig =
+        AwsConfig#aws_config {
+          access_key_id     = proplists:get_value(access_key_id, Creds),
+          secret_access_key = proplists:get_value(secret_access_key, Creds),
+          security_token    = proplists:get_value(session_token, Creds)
+         },
+
+    {FederatedConfig, Creds}.
 
 sts_query(AwsConfig, Action, Params) ->
     sts_query(AwsConfig, Action, Params, ?API_VERSION).
