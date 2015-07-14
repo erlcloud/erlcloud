2ccd4445ed0aad2d72a7c24c8e764771660ad853
Evgeny Bob <ebob@alertlogic.com>
2015-07-14 21:40:40 +0100

    update SDB to use SigV4

--- src/erlcloud_sdb.erl
+++ src/erlcloud_sdb.erl
@@ -365,8 +365,8 @@ sdb_request_with_retry(Config, Action, Params, Try, Timeout, StartTime) ->
 
 sdb_request_safe(Config, Action, Params) ->
     QParams = [{"Action", Action}, {"Version", ?API_VERSION}|Params],
-    case erlcloud_aws:aws_request_xml2(post, Config#aws_config.sdb_host,
-                                       "/", QParams, Config) of
+    case erlcloud_aws:aws_request_xml4(post, Config#aws_config.sdb_host,
+                                       "/", QParams, "sdb", Config) of
         {ok, Doc} ->
             {ok, Doc, [{box_usage, erlcloud_xml:get_float("/*/ResponseMetadata/BoxUsage", Doc)}]};
         {error, Error} ->
