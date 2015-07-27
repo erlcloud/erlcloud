a66034ee7035f50615811f0e4e537d232264e709
Konstantin Kuzmin <kkuzmin@alertlogic.com>
2015-07-27 16:33:18 +0100

    Report AWS statuses using http://status.aws.amazon.com/data.json

--- test/erlcloud_aws_tests.erl
+++ test/erlcloud_aws_tests.erl
@@ -7,7 +7,8 @@ request_test_() ->
      fun stop/1,
      [fun request_default_test/1,
       fun request_prot_host_port_str_test/1,
-      fun request_prot_host_port_int_test/1]}.
+      fun request_prot_host_port_int_test/1,
+      fun get_service_status_test/1]}.
 
 start() ->
     meck:new(erlcloud_httpc),
@@ -31,6 +32,65 @@ request_prot_host_port_int_test(_) ->
     ok = erlcloud_aws:aws_request(get, "http", "host1", 9999, "/path1", [], "id", "key"),
     Url = get_url_from_history(meck:history(erlcloud_httpc)),
     test_url(http, "host1", 9999, "/path1", Url).
+
+get_service_status_test(_) ->
+    StatusJsonS3 = jsx:encode(
+        [{<<"archive">>,
+            [[{<<"service_name">>,
+               <<"Amazon Simple Storage Service (US Standard)">>},
+              {<<"summary">>,
+               <<"[RESOLVED] Elevated errors for bucket operations in US-STANDARD ">>},
+              {<<"date">>,<<"1408497982">>},
+              {<<"status">>,1},
+              {<<"details">>,<<>>},
+              {<<"description">>,
+               <<"<div><span class=\"yellowfg\"> 6:46 PM PDT</span>&nbsp;We are investigatin">>},
+              {<<"service">>,<<"s3-us-standard">>}]
+            ]},
+         {<<"current">>,
+            [[{<<"service_name">>,
+               <<"Amazon Simple Storage Service (US Standard)">>},
+              {<<"summary">>,
+               <<"[RESOLVED] Elevated errors for bucket operations in US-STANDARD ">>},
+              {<<"date">>,<<"1408497982">>},
+              {<<"status">>,0},
+              {<<"details">>,<<>>},
+              {<<"description">>,
+               <<"<div><span class=\"yellowfg\"> 6:46 PM PDT</span>&nbsp;We are investigatin">>},
+              {<<"service">>,<<"s3-eu-central-1">>}]
+            ]}
+        ]
+    ),
+    OKStatusJson = jsx:encode(
+        [{<<"archive">>,
+            [[{<<"service_name">>,
+               <<"Amazon Simple Storage Service (US Standard)">>},
+              {<<"summary">>,
+               <<"[RESOLVED] Elevated errors for bucket operations in US-STANDARD ">>},
+              {<<"date">>,<<"1408497982">>},
+              {<<"status">>,1},
+              {<<"details">>,<<>>},
+              {<<"description">>,
+               <<"<div><span class=\"yellowfg\"> 6:46 PM PDT</span>&nbsp;We are investigatin">>},
+              {<<"service">>,<<"s3-us-standard">>}]
+            ]},
+         {<<"current">>,[]}
+        ]
+    ),
+
+    meck:expect(erlcloud_httpc, request, fun(_,_,_,_,_,_) -> {ok, {{200, "OK"}, [], StatusJsonS3}} end),
+    [S3Status] = erlcloud_aws:get_service_status("s3"),
+    meck:expect(erlcloud_httpc, request, fun(_,_,_,_,_,_) -> {ok, {{200, "OK"}, [], OKStatusJson}} end),
+    OKStatus = erlcloud_aws:get_service_status("sqs"),
+    meck:expect(erlcloud_httpc, request, fun(_,_,_,_,_,_) -> {ok, {{200, "OK"}, [], StatusJsonS3}} end),
+    OKStatusEC2 = erlcloud_aws:get_service_status("ec2"),
+    
+    ?debugFmt("OKStatusEC2 = ~p~n", [OKStatusEC2]),
+    [?_assertEqual(proplists:get_value(<<"status">>, S3Status), 0),
+     ?_assertEqual(proplists:get_value(<<"service">>, S3Status), <<"s3-eu-central-1">>),
+     ?_assertEqual(OKStatus, ok),
+     ?_assertEqual(OKStatusEC2, ok)
+     ].
 
 % ==================
 % Internal functions
