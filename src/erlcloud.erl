843c62096cfbf1b284f03433734e18bc59dfa951
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2015-09-07 07:59:34 +0000

    refactor: Use calling process; remove superfluous modules

--- src/erlcloud.erl
+++ src/erlcloud.erl
@@ -1,6 +1,6 @@
 -module(erlcloud).
 
--export([start/0, start_s3_multipart_sup/0, put_object_multipart/3]).
+-export([start/0]).
 
 -define(APP, erlcloud).
 
@@ -9,9 +9,3 @@ start() ->
     {ok, Apps} = application:get_key(?APP, applications),
     [application:start(App) || App <- Apps],
     application:start(?APP).
-
-start_s3_multipart_sup() ->
-    erlcloud_s3_multipart_sup:start_link().
-
-put_object_multipart(FilePath, Bucket, Key) ->
-    erlcloud_s3_multipart_server:put_object(FilePath, Bucket, Key).
