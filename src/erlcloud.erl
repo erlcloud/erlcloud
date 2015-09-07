0e715e51fad03563cb8df1b29e6027ca14fc6e1f
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2015-09-07 02:28:20 +0000

    Implement a simple manager for multi-part uploads

--- src/erlcloud.erl
+++ src/erlcloud.erl
@@ -1,5 +1,6 @@
 -module(erlcloud).
--export([start/0]).
+
+-export([start/0, start_s3_multipart_sup/0, put_object_multipart/3]).
 
 -define(APP, erlcloud).
 
@@ -8,3 +9,9 @@ start() ->
     {ok, Apps} = application:get_key(?APP, applications),
     [application:start(App) || App <- Apps],
     application:start(?APP).
+
+start_s3_multipart_sup() ->
+    erlcloud_s3_multipart_sup:start_link().
+
+put_object_multipart(FilePath, Bucket, Key) ->
+    erlcloud_s3_multipart_server:put_object(FilePath, Bucket, Key).
