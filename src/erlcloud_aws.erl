f036084dedb8cb8f169deba60be0a9dceb0cefb7
Konstantin Kuzmin <kkuzmin@alertlogic.com>
2015-11-11 10:18:22 +0000

    Retry 400 errors for S3.

--- src/erlcloud_aws.erl
+++ src/erlcloud_aws.erl
@@ -158,6 +158,8 @@ aws_request_form(Method, Protocol, Host, Port, Path, Form, Headers, Config) ->
            (#aws_request{response_type = error,
                          error_type = aws,
                          response_status = Status} = Request) when
+                %% Retry for 400, Bad Request is needed due to Amazon 
+                %% returns it in case of throttling
                     Status == 400; Status >= 500 ->
                 Request#aws_request{should_retry = true};
            (#aws_request{response_type = error} = Request) ->
--- src/erlcloud_s3.erl
+++ src/erlcloud_s3.erl
@@ -1190,7 +1190,9 @@ s3_result_fun(#aws_request{response_type = ok} = Request) ->
 s3_result_fun(#aws_request{response_type = error,
                            error_type = aws,
                            response_status = Status} = Request) when
-      Status >= 500 ->
+%% Retry for 400, Bad Request is needed due to Amazon 
+%% returns it in case of throttling
+      Status =:= 400; Status >= 500 ->
     Request#aws_request{should_retry = true};
 s3_result_fun(#aws_request{response_type = error, error_type = aws} = Request) ->
     Request#aws_request{should_retry = false}.
