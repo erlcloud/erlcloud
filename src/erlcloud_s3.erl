9c5e21b81ba2efe769c1b1f717d46c87e3d5715a
Konstantin Kuzmin <kkuzmin@alertlogic.com>
2015-11-13 17:35:16 +0000

    Refactor. Add tests.

--- src/erlcloud_s3.erl
+++ src/erlcloud_s3.erl
@@ -1127,7 +1127,7 @@ s3_request4_no_update(Config, Method, Bucket, Path, Subresource, Params, Body,
     S3Host = Config#aws_config.s3_host,
     AccessMethod = case Config#aws_config.s3_bucket_access_method of
         auto ->
-            case is_dns_compliant_name(Bucket) orelse
+            case erlcloud_util:is_dns_compliant_name(Bucket) orelse
                  Bucket == [] of 
                 true -> vhost; 
                 _ -> path
@@ -1341,14 +1341,4 @@ s3_endpoint_for_region(RegionName) ->
             "s3-external-1.amazonaws.com";
         _ ->
             lists:flatten(["s3-", RegionName, ".amazonaws.com"])
-    end.
-
--spec is_dns_compliant_name(string()) -> boolean().
-is_dns_compliant_name(Name) ->
-    RegExp = "^(([a-z0-9]|[a-z0-9][a-z0-9\\-]*[a-z0-9])\\.)*([a-z0-9]|[a-z0-9][a-z0-9\\-]*[a-z0-9])$",
-    case re:run(Name, RegExp) of
-        nomatch ->
-            false;
-        _ ->
-            true
     end.
--- src/erlcloud_util.erl
+++ src/erlcloud_util.erl
@@ -1,5 +1,6 @@
 -module(erlcloud_util).
--export([sha_mac/2, sha256_mac/2, md5/1, sha256/1]).
+-export([sha_mac/2, sha256_mac/2, md5/1, sha256/1,
+         is_dns_compliant_name/1]).
 
 sha_mac(K, S) ->
     try
@@ -35,4 +36,14 @@ md5(V) ->
     catch
         _:_ ->
             crypto:md5(V)
+    end.
+
+-spec is_dns_compliant_name(string()) -> boolean().
+is_dns_compliant_name(Name) ->
+    RegExp = "^(([a-z0-9]|[a-z0-9][a-z0-9\\-]*[a-z0-9])\\.)*([a-z0-9]|[a-z0-9][a-z0-9\\-]*[a-z0-9])$",
+    case re:run(Name, RegExp) of
+        nomatch ->
+            false;
+        _ ->
+            true
     end.
