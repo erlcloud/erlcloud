3d3c1e0ee06cf0334d2e5d7592d3780a422d2ec3
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2016-07-17 22:33:44 +0300

    add security token to config
    allow to define temporary security credentials to config object.

--- src/erlcloud.erl
+++ src/erlcloud.erl
@@ -1,6 +1,12 @@
 -module(erlcloud).
 -export([start/0]).
 
+-export([
+    new/2, new/3,
+    configure/2, configure/3
+]).
+
+-include("erlcloud_aws.hrl").
 -define(APP, erlcloud).
 
 start() ->
@@ -8,3 +14,62 @@ start() ->
     {ok, Apps} = application:get_key(?APP, applications),
     [application:start(App) || App <- Apps],
     application:start(?APP).
+
+
+%%
+%% create new erlcloud configuration 
+-spec new(string(), string()) -> aws_config().
+
+new(AccessKeyID, SecretAccessKey) ->
+    #aws_config{
+       access_key_id=AccessKeyID,
+       secret_access_key=SecretAccessKey
+      }.
+
+-spec new(string(), string(), string()) -> aws_config().
+
+new(AccessKeyID, SecretAccessKey, SecurityToken) ->
+    #aws_config{
+       access_key_id=AccessKeyID,
+       secret_access_key=SecretAccessKey,
+       security_token=SecurityToken
+      }.
+
+%%
+%% global erlcloud configuration
+-spec configure(string(), string()) -> ok.
+
+configure(AccessKeyID, SecretAccessKey) ->
+    case get(aws_config) of
+        undefined ->
+            put(aws_config, new(AccessKeyID, SecretAccessKey)),
+            ok;
+        #aws_config{} = Conf ->
+            put(aws_config, 
+                Conf#aws_config{
+                    access_key_id=AccessKeyID,
+                    secret_access_key=SecretAccessKey
+                }
+            ),
+            ok
+    end.
+
+-spec configure(string(), string(), string()) -> ok.
+
+configure(AccessKeyID, SecretAccessKey, SecurityToken) ->
+    case get(aws_config) of
+        undefined ->
+            put(aws_config, new(AccessKeyID, SecretAccessKey, SecurityToken)),
+            ok;
+        #aws_config{} = Conf ->
+            put(aws_config, 
+                Conf#aws_config{
+                    access_key_id=AccessKeyID,
+                    secret_access_key=SecretAccessKey,
+                    security_token=SecurityToken
+                }
+            ),
+            ok
+    end.
+
+
