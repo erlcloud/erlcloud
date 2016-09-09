c2db3b23acf0b0b36e7c25d2e76c28566c15641b
Nathaniel Waisbrot <nathaniel@waisbrot.net>
2016-09-09 11:19:17 -0400

    allow undefined expiration

--- include/erlcloud_aws.hrl
+++ include/erlcloud_aws.hrl
@@ -42,7 +42,7 @@
           secret_access_key::string()|undefined|false,
           security_token=undefined::string()|undefined,
           %% epoch seconds when temporary credentials will expire
-          expiration :: pos_integer(),
+          expiration=undefined :: pos_integer()|undefined,
           %% Network request timeout; if not specifed, the default timeout will be used:
           %% ddb: 1s for initial call, 10s for subsequence;
           %% s3:delete_objects_batch/{2,3}, cloudtrail: 1s;
