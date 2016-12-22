ef50a6dddf4b18361c91f3709fcfb0e4f1471e2c
Evgeny Bob <ebob@alertlogic.com>
2016-12-22 16:07:09 +0000

    bump things Forward

--- src/erlcloud_util.erl
+++ src/erlcloud_util.erl
@@ -7,60 +7,19 @@
 -define(MAX_ITEMS, 1000).
 
 sha_mac(K, S) ->
-    try
-        crypto:hmac(sha, K, S)
-    catch
-        error:undef ->
-            R0 = crypto:hmac_init(sha, K),
-            R1 = crypto:hmac_update(R0, S),
-            crypto:hmac_final(R1)
-    end.
+    crypto:hmac(sha, K, S).
+
 
 sha256_mac(K, S) ->
-    try
-        crypto:hmac(sha256, K, S)
-    catch
-        error:undef ->
-            R0 = crypto:hmac_init(sha256, K),
-            R1 = crypto:hmac_update(R0, S),
-            crypto:hmac_final(R1)
-    end.
+    crypto:hmac(sha256, K, S).
 
--ifdef(ERLANG_OTP_VERSION_14).
--else.
--ifdef(ERLANG_OTP_VERSION_15).
--else.
--define(ERLANG_OTP_VERSION_16_FEATURES, true).
--ifdef(ERLANG_OTP_VERSION_16).
--else.
--ifdef(ERLANG_OTP_VERSION_17).
--else.
--ifdef(ERLANG_OTP_VERSION_18).
--else.
--define(ERLANG_OTP_VERSION_19_FEATURES, true).
--endif.
--endif.
--endif.
--endif.
--endif.
-
--ifndef(ERLANG_OTP_VERSION_16_FEATURES).
-sha256(V) ->
-    crypto:sha256(V).
--else.
 sha256(V) ->
     crypto:hash(sha256, V).
--endif.
 
--ifndef(ERLANG_OTP_VERSION_16_FEATURES).
-md5(V) ->
-    crypto:md5(V).
--else.
 md5(V) ->
     crypto:hash(md5, V).
--endif.
 
--ifndef(ERLANG_OTP_VERSION_19_FEATURES).
+-ifndef(ERLANG_OTP_VERSION_19).
 rand_uniform(N) ->
     random:uniform(N).
 -else.
