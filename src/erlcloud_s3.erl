64414bfd0f2d98862def30f6472797c3f5856c5b
Aaron O'Hagan <aaron.ohagan@alertlogic.com>
2019-11-22 14:04:44 +0000

    add paris, stockholm, bahrain, hongkong, osaka to s3

--- src/erlcloud_s3.erl
+++ src/erlcloud_s3.erl
@@ -143,14 +143,21 @@ configure(AccessKeyID, SecretAccessKey, Host, Port, Scheme) ->
                                 | 'us-east-1'
                                 | 'us-east-2'
                                 | 'us-west-1'
+                                | 'us-west-2'
+                                | 'ca-central-1'
                                 | 'eu-west-1'
                                 | 'eu-west-2'
+                                | 'eu-west-3'
+                                | 'eu-north-1'
                                 | 'eu-central-1'
                                 | 'ap-south-1'
                                 | 'ap-southeast-1'
                                 | 'ap-southeast-2'
                                 | 'ap-northeast-1'
                                 | 'ap-northeast-2'
+                                | 'ap-northeast-3'
+                                | 'ap-east-1'
+                                | 'me-south-1'
                                 | 'sa-east-1'.
 
 
@@ -237,14 +244,20 @@ encode_location_constraint('us-east-1') -> undefined;
 encode_location_constraint('us-east-2') -> "us-east-2";
 encode_location_constraint('us-west-1') -> "us-west-1";
 encode_location_constraint('us-west-2') -> "us-west-2";
+encode_location_constraint('ca-central-1') -> "ca-central-1";
 encode_location_constraint('eu-west-1') -> "eu-west-1";
 encode_location_constraint('eu-west-2') -> "eu-west-2";
+encode_location_constraint('eu-west-3') -> "eu-west-3";
+encode_location_constraint('eu-north-1') -> "eu-north-1";
 encode_location_constraint('eu-central-1') -> "eu-central-1";
 encode_location_constraint('ap-south-1') -> "ap-south-1";
 encode_location_constraint('ap-southeast-1') -> "ap-southeast-1";
 encode_location_constraint('ap-southeast-2') -> "ap-southeast-2";
 encode_location_constraint('ap-northeast-1') -> "ap-northeast-1";
 encode_location_constraint('ap-northeast-2') -> "ap-northeast-2";
+encode_location_constraint('ap-northeast-3') -> "ap-northeast-3";
+encode_location_constraint('ap-east-1') -> "ap-east-1";
+encode_location_constraint('me-south-1') -> "me-south-1";
 encode_location_constraint('sa-east-1') -> "sa-east-1";
 encode_location_constraint(_) -> undefined.
 
