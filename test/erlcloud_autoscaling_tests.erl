7ce761e5e33f99e2e4fee9bddeb81b3f364e4218
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2020-07-06 14:06:32 +0100

    Fix dialyzer issues in erlcloud_autoscaling_tests.erl

--- test/erlcloud_autoscaling_tests.erl
+++ test/erlcloud_autoscaling_tests.erl
@@ -140,7 +140,7 @@ output_expect_seq(Responses) ->
 
 
 %% output_test converts an output_test specifier into an eunit test generator
--type output_test_spec() :: {pos_integer(), {string(), term()} | {string(), string(), term()}}.
+-type output_test_spec() :: {pos_integer(), {string(), term()} | {string(), string() | [string()], term()}}.
 -spec output_test(fun(), output_test_spec(), fun()) -> tuple().
 output_test(Fun, {Line, {Description, Response, Result}}, OutputFun) ->
     {Description,
